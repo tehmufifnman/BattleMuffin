ef0b16732a34234d04a8908f84bc3b8c7a6bf271
Brandon Nelson <theredraver@gmail.com>
2019-05-05 16:31:15 -0700

    Add initial version of request and request error handling

---
+++ src/Web/RequestResult.cs
@@ -0,0 +1,64 @@
+using System;
+
+namespace BattleMuffin.Web
+{
+    /// <summary>
+    ///     Represents a request made to the Blizzard API.
+    /// </summary>
+    /// <typeparam name="T">The type of request made.</typeparam>
+    public class RequestResult<T>
+    {
+        /// <summary>
+        ///    The requested object value from the Blizzard API.
+        /// </summary>
+        public T Value { get; }
+
+        /// <summary>
+        ///     The request error response from the Blizzard API.
+        /// </summary>
+        public RequestError Error { get; }
+
+        /// <summary>
+        ///     Indicates if the HTTP request succeeded.
+        /// </summary>
+        public bool Success { get; }
+
+        /// <summary>
+        ///     Initializes a request result with an object value.
+        /// </summary>
+        /// <param name="value">Instance of the Blizzard API request</param>
+        public RequestResult(T value)
+        {
+            Value = value;
+            Success = true;
+        }
+
+        /// <summary>
+        ///     Initializes a request result with an error.
+        /// </summary>
+        /// <param name="error">The error received from the Blizzard API request</param>
+        public RequestResult(RequestError error)
+        {
+            Error = error ?? throw new ArgumentNullException(nameof(error));
+            Success = false;
+        }
+
+        /// <summary>
+        ///     Implicit operator for initializing an object result with a value.
+        /// </summary>
+        /// <param name="value">Instance of the Blizzard API request</param>
+        public static implicit operator RequestResult<T>(T value)
+        {
+            return new RequestResult<T>(value);
+        }
+
+        /// <summary>
+        ///     Implicit operator for initializing an object result with an error.
+        /// </summary>
+        /// <param name="error">The error received from the Blizzard API request</param>
+        public static implicit operator RequestResult<T>(RequestError error)
+        {
+            return new RequestResult<T>(error);
+        }
+    }
+}
